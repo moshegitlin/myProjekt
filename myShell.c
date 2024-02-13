983e891bc585f70964dd6653260748cbc44f5bac
moshe <jmoshe052@gmail.com>
2024-02-13 11:00:42 +0200

    Initial commit

---
+++ myShell.c
@@ -0,0 +1,17 @@
+#include "myShell.h"
+#include "myFunction.h"
+int main()
+{
+    welcome();
+    while (1)
+    {
+        getLocation();
+        char* input = getInputFromUser();
+    }
+    return 0;
+}
+
+void welcome()
+{
+    puts("Welcome to my Shell");
+}
